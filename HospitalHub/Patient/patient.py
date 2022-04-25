44f605cd9a956aecd18851f28db1ac59c9dfc15e
Farahtharwat35 <65964974+Farahtharwat35@users.noreply.github.com>
2022-04-25 03:16:34 +0200

    Patient registration
    Set is_patient=true

---
+++ HospitalHub/Patient/patient.py
@@ -0,0 +1,41 @@
+from django.contrib.auth.models import AbstractBaseUser,BaseUserManager
+from django.db import models
+from django.conf import settings
+
+from django.contrib.auth.models import (
+    AbstractBaseUser, BaseUserManager
+)
+
+User = settings.AUTH_USER_MODEL
+
+class Patient:
+    
+    def register(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        email = request.POST["email"]
+        full_name = request.POST["full_name"]
+
+        # Ensure password matches confirmation
+        password = request.POST["password"]
+        confirmation = request.POST["confirmation"]
+        if password != confirmation:
+            return render(request, "auctions/register.html", {
+                "message": "Passwords must match."
+            })
+
+        # Attempt to create new user
+        try:
+            user = User.objects.create_user(username, email,full_name,
+                   password, is_owner=True,
+                  is_admin=False, is_doctor=False,is_staff=False,
+                  is_patient=True,city=None,phone_number)
+            user.save()
+        except IntegrityError:
+            return render(request, "auctions/register.html", {
+                "message": "Username already taken."
+            })
+        login(request, user)
+        return HttpResponseRedirect(reverse("index"))
+    else:
+        return render(request, "auctions/register.html")
